4b6728813472010592d8bbaa67038d6b993662ba
Simon Meins <info@csm-science.de>
2019-10-04 13:54:40 +0200

    Initial Commit

---
+++ quick-sort.py
@@ -0,0 +1,33 @@
+from random import randint
+
+def quick_sort(a):
+
+	'''
+	>>> quick_sort([99, 21, 79, 27, 92, 102])
+	[21, 27, 79, 92, 99, 102]
+	'''
+
+	l = len(a)
+	left =  []
+	equal = []
+	right = []
+
+	if l > 1:
+		pivotElement = a[randint(0, l - 1)]
+
+		for i in range(l):
+			if a[i] < pivotElement:
+				left.append(a[i])
+			elif a[i] == pivotElement:
+				equal.append(a[i])
+			elif a[i] > pivotElement:
+				right.append(a[i])
+
+		return quick_sort(left) + equal + quick_sort(right)
+
+	else:
+		return a
+
+if __name__ == '__main__':
+	import doctest
+	doctest.testmod()
