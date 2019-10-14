e64b89d46baa0f4c52aa092541f2b703c55cc066
ProgrammingSheran <51720237+ProgrammingSheran@users.noreply.github.com>
2019-10-14 23:41:32 +0200

    Functions to get lowest and highest element of a list written in Python

---
+++ max-min.py
@@ -0,0 +1,43 @@
+
+def get_min(a):
+
+    '''
+    Get lowest item of a list
+    :param a: Array given
+    :returns: Lowest element of the array given
+
+    >>> get_min([10, 102, 77])
+    10
+    '''
+
+    l = len(a)
+    for i in range(l - 1):
+        for j in range(l - i - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+
+    lowest_element = a[0]
+    return lowest_element
+
+def get_max(a):
+
+    '''
+    Get highest item of a list
+    :param a: Array given
+    :returns: Highest element of the array given
+
+    >>> get_max([7, 1, 99, 1782, 14])
+    1782
+    '''
+
+    l = len(a)
+    for i in range(l - 1):
+        for j in range(l - i - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+
+    return a[len(a) - 1]
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
